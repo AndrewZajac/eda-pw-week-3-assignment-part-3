051c3dbbae6812fac1f87760564380ad1a7f1578
AndrewZajac <andrewzajac05@gmail.com>
2024-04-04 11:58:03 -0500

    question 5 is completed, added newPart as a variable to my supplyChanges array

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -32,6 +32,10 @@ console.log('The number list now is', supplyChanges);
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
 
+let newPart = '25';
+supplyChanges.push(newPart);
+console.log('Added 25 to supplyChanges', supplyChanges);
+
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.' 
